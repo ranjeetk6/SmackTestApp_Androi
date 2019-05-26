d64deca5bc681d4faf9f083d377c9948fcafe8a8
ranjeet <ranjeetsamsung@gmail.com>
2019-05-26 16:45:48 +0530

    lessionLogin

--- app/src/main/java/com/example/smacktest/MainActivity.kt
+++ app/src/main/java/com/example/smacktest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smacktest
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.View
+import com.example.smacktest.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,8 +45,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClick(view:View){
-
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClick(view:View){
